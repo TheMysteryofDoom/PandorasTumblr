91fedf99fc7c7bc9e9a48695d337319d818d8ccf
TheMysteryofDoom <themysteryofdoom@gmail.com>
2018-04-08 16:44:58 +0800

    Will probably start brute forcing

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -1,5 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
 public class ViewController {
 
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register() {
+		
+		//Test
+		System.out.println("Help");
+		
+        return "index";
+    }
 }
